8d0ec1bb19730a34f2edf258967112c1f166c6fc
Listo806 <listoqasa@proton.me>
2025-10-22 17:44:38 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // 👈 this will be your full platform layout
+import App from "./App.jsx"; // this loads your full platform layout
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
