7728f04a44f3901163848fb7c6d32aeacbad0e2b
Listo806 <listoqasa@proton.me>
2025-10-22 16:32:10 -0500

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,4 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugi=
+  plugins: [react()],
+  build: {
+    outDir: "dist"
+  }
+});
