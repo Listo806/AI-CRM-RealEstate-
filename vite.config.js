481ecd49866344fa73b650d01af1a0ee835a5c61
Listo806 <listoqasa@proton.me>
2025-10-22 10:03:59 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 export default defineConfig({
   plugins: [react()],
+  root: "client",        // tells Vite your app is inside the "client" folder
   build: {
-    outDir: "dist"
-  }
+    outDir: "../dist",   // tells it to save the final version in a "dist" folder for Vercel
+    emptyOutDir: true,
+  },
 });
