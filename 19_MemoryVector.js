369a27c4db9797ddc230ce065fb879669b501a98
Listo806 <listoqasa@proton.me>
2025-10-21 22:14:52 -0500

    Update 19_MemoryVector.js

--- 19_MemoryVector.js
+++ 19_MemoryVector.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const MemoryVectorSchema = new mongoose.Schema({
+  userId: String,
+  text: String,
+  tags: [String],
+  vector: { type: [Number], index: false },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('MemoryVector', MemoryVectorSchema);
 
