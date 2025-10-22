abefe0348949f81c04364df033e6afbb8faa11f6
Listo806 <listoqasa@proton.me>
2025-10-22 17:22:49 -0500

    Delete client/src/pages/main.jsx

--- client/src/pages/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import Phase10LiveTimelineWithAlerts from "./pages/Phase10LiveTimelineWithAlerts.jsx";
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <Phase10LiveTimelineWithAlerts />
-  </React.StrictMode>
-);
