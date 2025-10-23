5486a2a8cd35679fbfd28044dceaf63dab43a8ca
Listo806 <listoqasa@proton.me>
2025-10-22 21:40:00 -0500

    Update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx"; // this loads your full platform layout
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
