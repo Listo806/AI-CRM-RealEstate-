7bab3822dcdab3153193f984e74ca50d73a573f8
Listo806 <listoqasa@proton.me>
2025-10-22 21:14:06 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,21 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header.jsx";
 import Footer from "./components/Footer.jsx";
 import Landing from "./pages/Landing.jsx";
+import SignIn from "./pages/SignIn.jsx";
+import Dashboard from "./pages/Dashboard.jsx";
 
 export default function App() {
   return (
     <Router>
-      <div className="flex flex-col min-h-screen">
-        <Header />
-        <main className="flex-grow">
-          <Routes>
-            <Route path="/" element={<Landing />} />
-          </Routes>
-        </main>
-        <Footer />
-      </div>
+      <Header />
+      <main className="pt-20">
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </main>
+      <Footer />
     </Router>
   );
 }
