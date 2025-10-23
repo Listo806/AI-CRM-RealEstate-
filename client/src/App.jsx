4e2cda7af9fc79f1d0dcd627c3ed7d865a9e0d11
Listo806 <listoqasa@proton.me>
2025-10-22 19:44:41 -0500

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,18 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing.jsx";
+import SignIn from "./pages/SignIn.jsx";
 
 export default function App() {
-  return <Landing />;
+  return (
+    <Router>
+      <Routes>
+        {/* Homepage */}
+        <Route path="/" element={<Landing />} />
+        
+        {/* Sign In Page */}
+        <Route path="/signin" element={<SignIn />} />
+      </Routes>
+    </Router>
+  );
 }
